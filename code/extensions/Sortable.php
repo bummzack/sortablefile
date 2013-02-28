e3ec1305811b328a22ca9d1b705497263f0295eb
Roman <bummzack@gmail.com>
2013-02-28 16:30:27 +0100

    Re-Adding the Sortable DataExtension. Maybe somebody still needs sortable has_many support, where this class can come in handy.

---
+++ code/extensions/Sortable.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Sortable data-extension
+ * Currently this only adds the required "SortOrder" column to the DB. 
+ * 
+ * @author bummzack
+ */
+class Sortable extends DataExtension
+{
+	public static $db = array(
+		'SortOrder' => 'Int'
+	);
+	
+	public static $sort_dir = 'ASC';
+	
+	/**
+	 * @see DataExtension::augmentSQL()
+	 */
+	public function augmentSQL(SQLQuery &$query)
+	{
+		$select = $query->getSelect();
+		if(
+			empty($select) || 
+			$query->getDelete() || 
+			in_array("COUNT(*)",$select) || 
+			in_array("count(*)",$select)
+		){ return; }
+		
+		$query->setOrderBy("\"SortOrder\" " . self::$sort_dir);
+	}
+	
+	/**
+	 * Assign a sort number when object is written
+	 * @see DataExtension::onBeforeWrite()
+	 */
+	public function onBeforeWrite()
+	{
+		if(!$this->owner->ID || !$this->owner->SortOrder) {
+			$classes = ClassInfo::dataClassesFor($this->owner->ClassName);
+		    $sql = new SQLQuery('count(ID)', array_shift($classes));
+		    $val = $sql->execute()->value();
+		    $this->owner->SortOrder = is_numeric($val) ? $val+1 : 1;
+		}
+	}	
+}
